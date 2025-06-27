623016d80b3ee241abc05d8201c5b0858e72ae1b
Ryan Krattiger <ryan.krattiger@kitware.com>
2025-06-27 11:37:56 -0400

    Deploy and updates 2025.02

--- spack/configs/frontier/deploy.sh
+++ spack/configs/frontier/deploy.sh
@@ -1,53 +1,58 @@
 #!/bin/bash
 set -e
 
-DEPLOY_PREFIX="${PREFIX:-/sw/frontier/ums/ums032/spack}"
-SDK_DEPLOY_VERSION=$(date +%y.%m.%d)
+export DEPLOY_PREFIX="${PREFIX:-/sw/frontier/ums/ums032/spack}"
+export SDK_DEPLOY_VERSION=$(date +%y.%m.%d)
 
-function append_version_info()
+version_info()
 {
   dir=$1
-  out=$2
 
-  pushd $dir
+  pushd $dir > /dev/null
 
-  if [ $(git status) ]; then
-    if [ $(git describe) ]; then
-      git describe >> $out
+  if [[ $(git status &> /dev/null) ]]; then
+    if [[ $(git describe &> /dev/null) ]]; then
+      git describe
     else
-      git rev-parse HEAD >> $out
+      git rev-parse HEAD
     fi
   else
-    echo "$(realpath $dir)" >> $out
+    echo "$(realpath $dir)"
   fi
 
-  popd
+  popd > /dev/null
 }
 
 # Configure working directory for this deployment
 mkdir -p ${DEPLOY_PREFIX}/${SDK_DEPLOY_VERSION}
+cd ${DEPLOY_PREFIX}/${SDK_DEPLOY_VERSION}
 
+echo "$PWD"
 # Clear the deployment manifest
-log="manifest.txt"
+log="${DEPLOY_PREFIX}/${SDK_DEPLOY_VERSION}/manifest.txt"
 echo -n > $log
 
-# Collect the configuration files to use
+echo "Initializing Facility configs..."
 if [ -z $FACILITY_CONFIG_ROOT ]; then
   local facility_config_tag=${FACILITY_CONFIG_TAG:-master}
   git clone --depth 1 --branch ${facility_config_tag} git@github.com:E4S-Project/facility-external-spack-configs.git
   FACILITY_CONFIG_ROOT=$PWD/facility-external-spack-configs
-if
+fi
+export FACILITY_CONFIG_ROOT
 echo "facility-external-spack-configs" >> $log
-append_version_info "$FACILITY_CONFIG_ROOT" "$log"
+version_info "$FACILITY_CONFIG_ROOT" >> "$log"
 
+echo "Initializing DAV SDK configs..."
 if [ -z $DAV_SDK_CONFIG_ROOT ]; then
   local dav_sdk_config_tag=${DAV_SDK_CONFIG_TAG:-main}
   git clone --depth 1 --branch ${dav_sdk_config_tag} git@github.com:DAV-SDK/davsdk.git
   DAV_SDK_CONFIG_ROOT=$PWD/davsdk
 fi
+export DAV_SDK_CONFIG_ROOT
 echo "dav-configs" >> $log
-append_version_info "$DAV_SDK_CONFIG_ROOT" "$log"
+version_info "$DAV_SDK_CONFIG_ROOT" >> "$log"
 
+echo "Initializing Spack..."
 # Configure Spack for deployment
 if [ -z $SPACK_ROOT ]; then
   local spack_tag=${SPACK_TAG:-develop}
@@ -59,14 +64,23 @@ else
 fi
 spack debug report >>  "$log"
 
-spack env activate ${DAV_SDK_CONFIG_ROOT}/spack/configs/frontier
+echo $log
+cat $log
+read
 
-spack config blame
+echo "Activating environment at ... ${DAV_SDK_CONFIG_ROOT}/spack/configs/frontier"
+spack env activate ${DAV_SDK_CONFIG_ROOT}/spack/configs/frontier
+spack env status
+
+spack config blame &> ${DEPLOY_PREFIX}/${SDK_DEPLOY_VERSION}/configs.blame.txt
 echo "Verify config..."
 read
 
-spack concretize -f
+echo "Concretizing..."
+spack concretize -f |& tee ${DEPLOY_PREFIX}/${SDK_DEPLOY_VERSION}/concrete.txt
 echo "Verify Concretization..."
 read
 
-spack install -j 8
+spack -t install -j 8 |& tee ${DEPLOY_PREFIX}/${SDK_DEPLOY_VERSION}/install.txt
+  
+spack module lmod refresh --delete-tree -y

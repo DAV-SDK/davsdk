76d640615adb64353e25656445d4269355d796ee
Vicente Adolfo Bolea Sanchez <vicente.bolea@kitware.com>
2026-03-12 12:11:34 -0400

    Revert "ci: add maintenance build jobs for one-time source rebuild with padding"
    This reverts commit 4377b64f61e4fefdc1a1fa89b56e3efe16dc3cd5.

--- scripts/ci/gitlab-ci/run.sh
+++ scripts/ci/gitlab-ci/run.sh
@@ -108,8 +108,7 @@ case ${STEP} in
     spack config blame
 
     # Install the environment with timing and parallel jobs
-    # shellcheck disable=SC2086
-    spack -t install "-j$((NUM_CORES*2))" --show-log-on-error --no-check-signature --fail-fast ${SPACK_INSTALL_FLAGS:-} | tee spack_log.out 2>&1
+    spack -t install "-j$((NUM_CORES*2))" --show-log-on-error --no-check-signature --fail-fast | tee spack_log.out 2>&1
 
     # Push installed packages to buildcache with padded paths so deploy
     # can relocate them to the persistent Lustre install_tree
